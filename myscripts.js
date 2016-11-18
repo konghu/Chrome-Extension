6cd88b1e415223c092cc45a55954e978e2b3928c
konghu <zhongkong.hu@gmail.com>
2016-11-17 20:24:12 -0500

    17th
    With completed histogram

--- myscripts.js
+++ myscripts.js
@@ -1,65 +1,3 @@
-// // var content=document.getElementsByClassName("userContent");  // Find the elements
-// // var x = $(".userContent").find("p").first().html();
-// // var x = document.getElementsByClassName("userContent").getElementsByTagName("p");
-//
-// var x = document.getElementsByTagName("p");
-//
-//
-// $(document).ready(function(){
-//
-//     for(var i = 0; i < x.length; i++){
-//
-//         $("<button class='icon' id='iconid"+i+"'>check</button>").insertAfter(x[i]);
-//
-//     }
-//
-//     $(".icon").click(function(){
-//
-//         // create boxes and append text to boxes
-//         order = parseInt(this.id.charAt(6));
-//         $("<div class='box' id='boxid"+order+"'>"+x[order].innerText+"</div>").insertAfter('#'+this.id+'');
-//     });
-//
-//
-//         $(document).on("click", ".icon", function(){
-//             var url = "https://www.googleapis.com/language/translate/v2?key=AIzaSyD4rLhLb3ZmwjLJDt-njNqFYP30eHeaBTQ&target=en&q=";
-//
-//             var order = parseInt(this.id.charAt(6));
-//
-//             var content = x[order].innerText;
-//             $("#boxid"+order+"").append("<div id='translation-box"+order+"' style='background-color: lightblue; position:absolute; width:200px; height:100px'; </div>");
-//
-//             $("<div class='emotionbox' id='emotionboxid"+order+"'></div>").insertAfter('#'+this.id+'');
-//             $("<div class='sentimentbox' id='sentimentboxid"+order+"'></div>").insertAfter('#'+this.id+'');
-//
-//             console.log(content);
-//             $.get(url + content, function(data){
-//                 console.log(data);
-//                 var translation = data.data.translations[0].translatedText;
-//                 $("#translation-box"+order+"").html(translation);
-//
-//
-//
-//                 var url2 = "https://still-lowlands-64290.herokuapp.com/pjsabc";
-//
-//                     $.post(url2, {text: translation}, function(data){
-//                         console.log(JSON.stringify(data.docEmotions));
-//                         $("#emotionboxid"+order+"").html(JSON.stringify(data.docEmotions));
-//
-//
-//
-//                         // $("#emotionboxid"+order+"").html(JSON.stringify(data.docEmotions));
-//                     });
-//
-//
-//                 var url3 = "https://scary-ghoul-77952.herokuapp.com/pjsabc";
-//                     $.post(url3, {text: translation}, function(data){
-//                         console.log(JSON.stringify(data.docSentiment));
-//                         $("#sentimentboxid"+order+"").html(JSON.stringify(data.docSentiment));
-//                     });
-//             });
-//     });
-// });
 
 document.documentElement.style.height = '100%';
 document.body.style.height = '100%';
@@ -70,21 +8,13 @@ document.body.style.width = '100%';
 var div = document.createElement( 'div' );
 var btnForm = document.createElement( 'form' );
 var btn = document.createElement( 'input' );
+var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg", style= 'width:400px; height:300px');
+
 
 //append all elements
 document.body.appendChild( div );
 div.appendChild( btnForm );
 btnForm.appendChild( btn );
-//set attributes for div
-// div.id = 'myDivId';
-// div.style.position = 'fixed';
-// div.style.top = '50%';
-// div.style.left = '50%';
-// div.style.width = '100%';
-// div.style.height = '100%';
-// div.style.backgroundColor = 'red';
-
-//set attributes for btnForm
 btnForm.action = '';
 
 //set attributes for btn
@@ -95,12 +25,13 @@ btn.style.position = 'absolute';
 btn.style.top = '50%';
 btn.style.left = '50%';
 
+
 $(document).ready(function() {
-    $("body").append("<div id='hover-box' style='background-color: yellow; position:absolute; width:100px; height:100px'></div>");
+    $("body").append("<div id='hover-box' style='background-color: none; position:absolute; width:100px; height:100px; border: 2px solid #169BD5; border-radius: 10px 0px 0px 10px;'></div>");
 
-    $("body").append("<div id='translation-box' style='background-color: lightblue; position:absolute; width:200px;'></div>");
+    $("body").append("<div id='translation-box' style='position:absolute; width:200px;'></div>");
 
-    $("body").append("<div id='emotion-box' style='z-index: 1000000000;background-color: indianred; width:200px; height:100px ' ></div>");
+    $("body").append("<div id='emotion-box' style='z-index: 1000000000; width:200px; height:100px'></div>");
 
     $(document).on("click", ".userContent", function () {
         var position = $(this).offset();
@@ -114,20 +45,18 @@ $(document).ready(function() {
 
         $("#translation-box").css("left", (position.left + width) + "px");
         $("#translation-box").css("top", position.top + "px");
-        $("#translation-box").css("height", "500px");
         $("#translation-box").css("z-index:10000000");
 
-        // var x =$("#translation-box").offset();
-        // $("#emotion-box").css("left", (x.left + width) + "px");
-        // $("#emotion-box").css("top", x.top + "px");
-        // $("#emotion-box").css("height", $("#translation-box").height() + "px");
-        // $("#emotion-box").css("z-index:10000000");
 
-        $("#emotion-box").appendTo("#translation-box");
+        // var svg = document.createElement('SVG');
+
+        // document.body.appendChild(svg);
+
+
 
 
         console.log($(this).html());
-        var content = $(this).find("p").first().html();
+        var content = $(this).find("p").text();
         console.log(content);
         var url1 = "https://www.googleapis.com/language/translate/v2?key=AIzaSyD4rLhLb3ZmwjLJDt-njNqFYP30eHeaBTQ&target=en&q=";
         $.get(url1 + content, function (data) {
@@ -140,8 +69,90 @@ $(document).ready(function() {
 
             $.post(url2, {text: translation}, function (data) {
                 $("#emotion-box").html(JSON.stringify(data.docEmotions));
-                console.log(data.docEmotions);
+
+
+
+                var emotionData = [data.docEmotions.anger *400, data.docEmotions.sadness *400, data.docEmotions.joy *400, data.docEmotions.fear *400,data.docEmotions.disgust*400];
+                console.log(emotionData);
+                var barWidth = 35;
+                var barOffset = 5;
+
+
+                var myChart = d3.select('#translation-box').append('svg')
+                    .attr('id', "graph")
+                    .attr('width', 200)
+                    .attr('height', 300)
+                    .style('background', '#ffffff')
+                    .selectAll('rect')
+                    .data(emotionData)
+                    .enter().append('rect')
+                    .style('fill', 'lightblue')
+                    .attr('width',barWidth)
+                    .attr('height', function(d){
+                        return d;
+                    })
+                    .attr('x', function(d,i){
+                        return i * (barWidth + barOffset
+                            );
+                    })
+                    .attr('y', function(d){
+                        return 300 - d;
+                    });
+
+
+                var img = document.createElement("img");
+                img.setAttribute("id", "anger");
+                img.setAttribute("class", "labels");
+                img.src = chrome.extension.getURL("images/Anger.png");
+                var src = document.getElementById("translation-box");
+                src.appendChild(img);
+
+
+                var img = document.createElement("img");
+                img.setAttribute("id", "sadness");
+                img.setAttribute("class", "labels");
+                img.src = chrome.extension.getURL("images/Sadness.png");
+                var src = document.getElementById("translation-box");
+                src.appendChild(img);
+
+
+                var img = document.createElement("img");
+                img.setAttribute("id", "joy");
+                img.setAttribute("class", "labels");
+                img.src = chrome.extension.getURL("images/Joy.png");
+                var src = document.getElementById("translation-box");
+                src.appendChild(img);
+
+                var img = document.createElement("img");
+                img.setAttribute("id", "fear");
+                img.setAttribute("class", "labels");
+                img.src = chrome.extension.getURL("images/Fear.png");
+                var src = document.getElementById("translation-box");
+                src.appendChild(img);
+
+                var img = document.createElement("img");
+                img.setAttribute("id", "disgust");
+                img.setAttribute("class", "labels");
+                img.src = chrome.extension.getURL("images/Disgust.png");
+                var src = document.getElementById("translation-box");
+                src.appendChild(img);
+
+
+
+
+
+
+
+
+
+
             });
+            // var img = new Image();
+            // $("svg").append("img");
+            // img.src = chrome.extension.getURL("images/Anger.png");
+
+
+
 
 
             var url3 = "https://scary-ghoul-77952.herokuapp.com/pjsabc";
@@ -157,3 +168,15 @@ $(document).ready(function() {
         });
     });
 });
+
+// $(window).on('load', function(){
+//     alert('hahaha');
+//     var img1 = document.createElement("img1");
+//     img1.src = chrome.extension.getURL("images/Anger.png");
+//     //alert('ddd');
+//     var src = document.getElementById("graph");
+//     console.log(src);
+//     src.appendChild(img1);
+//
+//
+// });
